5f130427b6a04066358e8f445624c23dbb6dfad5
Natalia Oner <64476423+omegaaq@users.noreply.github.com>
2022-11-12 15:04:57 +0300

    Add files via upload

---
+++ lesson2_into.py
@@ -0,0 +1,137 @@
+
+# windows+shif+s frame for screenshooots
+# ctrl+/ comments - uncomments
+
+
+# print('hello world!')
+# print('5')
+# print('5')
+
+# #1print('`') # це апостроф
+# \n back slash for new line - з нового рядка
+# print('5\n4\n3')
+
+##2 end - що в кінці прінта має бути і знімає перенесення
+# # print('543','hello')
+# print('hello', end=' ')
+# print('world', end='!')
+
+# #3separators
+# print('hello', 'all', 'world', sep='_')
+# print('hello', 'all', 'world', sep='*')
+#print('hello', 'all', 'world', sep='*', end='\n\n\n')
+
+# #4\t tabulation рядковий літерал
+#print('\thello', 'all', 'world', sep='*')
+
+# #5 *
+#print('hello ' * 4)
+
+# #6 + операція склеювання стрічки
+# print('hello ' + 'world')
+# print('hello' + ' ' + 'world')
+# print('5' + '4', '4' + '2')  #54  42
+
+ # #7типи даних:
+#int float string boolean variables
+
+# #8Variables
+# x = 4
+# x1 = '121'
+# x2 = 0.32
+# my_name = 'nata'
+# myName = 'nata'
+# name = 'vlad'
+# age = 15
+# age1 = 1
+
+#print(name, '\n', age)
+#print('name: ', name, 'age:', age)
+##print('name: ' + name + 'age:' + age)  # !!! its a mistake! data will be parse(skleyuvatus)
+#print('name: ', name, '\n', 'age: ', age)
+#print(age + age1) ## its not work!
+#print('name'+ age) ## its wrong
+# print(f'Name: {name} and my age : {age}')
+# print(f'Name: {name} and my age : {age + 10}')
+
+
+# # #9how to learn type of data
+# print(type(x))
+# print(type(x1))
+# print(type(x2))
+
+# #10 wrong!
+# print(k)
+# k = 1
+
+# #11print('`') # це апостроф
+
+# ## 12review
+# name = 'natalia'
+# surname = 'oner'
+# age = '35'
+# biografy = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit\n, ' \
+#            'sed do eiusmod tempor incididunt ut labore et dolore magna\n ' \
+#            'aliqua. Ut enim ad minim veniam, quis nostrud exercitation\n ' \
+#            'ullamco laboris nisi ut aliquip ex ea commodo consequat. \n' \
+#            ' Duis aute irure dolor in reprehenderit in voluptate velit esse\n ' \
+#            'cillum dolore eu fugiat nulla pariatur.\n' \
+#            ' Excepteur sint occaecat cupidatat non proident,\n' \
+#            ' sunt in culpa qui \n' \
+#            'officia deserunt mollit anim id est laborum.\n'
+
+#print(f' my name is: {name}\n my surname is: {surname}\n my age is: {age}\n my biografy is: {biografy}')
+
+
+# ##13
+# name = 'natalia'
+# phone = '093 123 123 123'
+# age = 35
+# pin_code = '0001'
+# print('name: ', end='')
+# print(name)
+# print('age: ', age)
+# #print('phone number: '+phone)
+# print(f'name: {name}\nage: {age}\nphone: {phone}')
+
+# ##14
+# name = 'natalia'
+# phone = '093 123 123 123'
+# age = 35
+# #print(name+age) ##its wrong
+# print(name+str(age))
+
+##15
+# x = '4'
+# y = 3
+# print(int(x)+y)
+
+##16 ---- input ----1
+# text = 'enter'
+# name = input(f'{text} some information: ')
+# #x = input(f'{text} some information: ')
+# #print(input(f'{text} some information: '))
+# #print('my name=' + input(f'{text} some information: '))
+# print('my name is: ' +name)
+# print(f'bye {name}')
+
+##17
+# x = input('x: ')
+# y = input('y: ')
+# print(x + y)
+# print(int(x) + int(y))
+
+##18
+x = int(input('x: '))
+y = int(input('y: '))
+print(x + y)
+
+
+
+
+
+
+
+
+
+
