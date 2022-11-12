ec089ff2eaec4a61864c979d459541f1d73144ca
Natalia Oner <64476423+omegaaq@users.noreply.github.com>
2022-11-12 15:06:01 +0300

    Add files via upload

---
+++ lesson4_step.py
@@ -0,0 +1,58 @@
+
+# 1 #Eaqual number
+# number =int( input('enter num:'))
+# if number % 2==0:
+#     print('number is equal')
+# else:
+#     print('number is not equal')
+
+# 2
+# color = input('enter color : red blur or green:')
+# if color == 'red':
+#     print('red')
+# else:
+#     if color == 'blue':
+#         print('blue')
+#     else:
+#         if color == 'green':
+#             print('green')
+
+# # 3
+# color = input('enter color : red blur or green:')
+# if color == 'red':
+#     print('red')
+# elif color == 'blue':
+#     print('blue')
+# elif color == 'green':
+#     print('green')
+# else:
+#     print('incorrect color')
+
+# # 4
+# day = input('enter day:')
+# if day == 'monday' and day == 'tuesday' and day == 'wednesday':
+#     print('correct')
+
+
+# # 5
+# day = input('enter day:')
+# if day == 'monday' or day == 'tuesday' or day == 'wednesday':
+#     print('correct')
+
+# # 6
+# login = input('login:')
+# password = input('enter password:')
+# if login == 'a' and password == '1':
+#     print('wellcome')
+# else:
+#     print('error')
+
+# 7
+x = 52
+print(52 % 2) #52/2 = 26 (-> 0)
+print(52 % 5) #52/5 = 10 (50 + 2 -> 2)
+
+
+
+
+
